9f5a3b19e688e90aa27e39f2b29b8befae3a4610
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-10-12 14:47:36 +0800

    new

---
+++ js/music-list.js
@@ -0,0 +1,51 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "All too Well",
+      artist: "Taylor Swift",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Nothings gonna change my love for you",
+      artist: "Justin Bieber",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "I'm not the only One",
+      artist: "Sam Smith",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Happier",
+      artist: "Ed Sheeran",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Save your Tears",
+      artist: "Ariana Grande ft. The Weekend",
+      img: "music-5",
+      src: "music-5"
+    },
+  
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
